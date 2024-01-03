48a4d6527d2086ba1b4a8bebef0402cc5083e293
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-03 21:35:05 +0500

    Creating configurations

---
+++ WorldMarket/WorldMarket.Infrastructure/Persistence/Configurations/SaleItemEntitiyConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using WorldMarket.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WorldMarket.Infrastructure.Persistence.Configurations
+{
+    internal class SaleItemEntitiyConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.ToTable(nameof(SaleItem));
+
+            builder.HasKey(si => si.Id);
+
+            builder.Property(si => si.Quantity)
+                .IsRequired()
+                .HasColumnType("integer");
+
+            builder.Property(si => si.UnitPrice)
+                .HasColumnType("money");
+
+            builder.HasOne(si => si.Sale)
+                .WithMany(s => s.SaleItems)
+                .HasForeignKey(si => si.SaleId);
+
+            builder.HasOne(si => si.Product)
+                .WithMany(p => p.SaleItems)
+                .HasForeignKey(si => si.ProductId);
+        }
+    }
+}
---
+++ WorldMarket/WorldMarket.Infrastructure/Persistence/Configurations/SupplyItemEntityConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using WorldMarket.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldMarket.Infrastructure.Persistence.Configurations
+{
+    internal class SupplyItemEntityConfiguration : IEntityTypeConfiguration<SupplyItem>
+    {
+        public void Configure(EntityTypeBuilder<SupplyItem> builder)
+        {
+            builder.ToTable(nameof(SupplyItem));
+
+            builder.HasKey(si => si.Id);
+
+            builder.Property(si => si.Quantity)
+                .IsRequired()
+                .HasColumnType("integer");
+
+            builder.Property(si => si.UnitPrice)
+                .HasColumnType("money");
+
+            builder.HasOne(si => si.Supply)
+                .WithMany(s => s.SupplyItems)
+                .HasForeignKey(si => si.SupplyId);
+
+            builder.HasOne(si => si.Product)
+                .WithMany(p => p.SupplyItems)
+                .HasForeignKey(si => si.ProductId);
+        }
+    }
+}
