b42a63a05730cf654cf04d94d9f132af52c761ed
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-01-03 22:28:06 +0500

    Creating DbContext

---
+++ WorldMarket/WorldMarket.Infrastructure/Persistence/WorldMarketDbContext.cs
@@ -0,0 +1,35 @@
+﻿using WorldMarket.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldMarket.Infrastructure.Persistence
+{
+    public class WorldMarketDbContext : DbContext
+    {
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Sale> Sales { get; set; }
+        public virtual DbSet<SaleItem> SaleItems { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<Supply> Supplies { get; set; }
+        public virtual DbSet<SupplyItem> SupplyItems { get; set; }
+
+        public WorldMarketDbContext(DbContextOptions<WorldMarketDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
